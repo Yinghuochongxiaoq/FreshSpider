6a7067a0710f1ae0c773b3066f13c5f84ffe6fa5
FreshMan <qinbocai@sina.cn>
2019-12-17 16:07:39 +0800

    Update send_sms.py

--- com/freshman/pyexercise/daily_exercise/send_sms.py
+++ com/freshman/pyexercise/daily_exercise/send_sms.py
@@ -1,13 +1,13 @@
 #pip install twilio 安装包
 from twilio.rest import Client
 
-account_sid = "AC646ce60346d5ae69004260e4b185cd4b"
-auth_token = "63bfee3ec94c95df75e4c2d66a1d03b3"
+account_sid = ""
+auth_token = ""
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+8618883257311",
-    from_="+18508803122",
+    to="+861888888888888888888",
+    from_="+1888888888888803122",
     body="早饭开始了"
 )
 
